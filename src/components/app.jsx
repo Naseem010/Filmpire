0d97f4eeda2edde44f95687e4f7e33523116aba3
Faheemuddin Naseem <fnaseem010@gmail.com>
2023-03-09 18:52:02 +0530

    file and folder structure

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import { CssBaseline } from '@mui/material';
-// import { Router, Switch } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import { Actors, MovieInformation, Movies, NavBar, Profile } from './index';
+import useStyles from './styles';
 
-const App = () => (
-  <div>
-    <CssBaseline />
-    <h1>Hello world-Filmistan</h1>
-  </div>
-);
+function App() {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <NavBar />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Routes>
+          <Route path="/" element={<Movies />} />
+          <Route path="/movie/:id" element={<MovieInformation />} />
+          <Route path="/actors/:id" element={<Actors />} />
+          <Route path="/profile/:id" element={<Profile />} />
+
+        </Routes>
+      </main>
+    </div>
+  );
+}
 
 export default App;
