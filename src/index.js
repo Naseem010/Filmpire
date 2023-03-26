304210cb227e21f5457fb4c516b9de36a172e176
Faheemuddin Naseem <fnaseem010@gmail.com>
2023-03-26 11:08:18 +0530

    adding redux to application

--- src/index.js
+++ src/index.js
@@ -2,14 +2,18 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { Provider } from 'react-redux';
 import App from './components/app';
+import store from './app/store';
 
 const theme = createTheme();
 ReactDom.render(
-  <ThemeProvider theme={theme}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ThemeProvider>,
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
+  </Provider>,
   document.getElementById('root'),
 );
